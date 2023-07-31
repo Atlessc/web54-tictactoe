aa36d9209494fd738065ae60377034ae2dd6a2cb
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-07-31 22:35:08 +0000

    afdgdnhfndfsr

--- tictactoe-game/src/components/GameStatus.jsx
+++ tictactoe-game/src/components/GameStatus.jsx
@@ -7,7 +7,7 @@ function GameStatus({ currentPlayer }) {
         className={`w-4 h-4 mr-2 transform transition-transform duration-500 ${
           currentPlayer === 'X' ? 'scale-x-100' : 'scale-x-0'
         }`}
-        viewBox="0 0 40 40"
+        viewBox="0 0 20 20"
       >
         <path
           stroke="#4B5563"
@@ -20,7 +20,7 @@ function GameStatus({ currentPlayer }) {
         className={`w-4 h-4 transform transition-transform duration-500 ${
           currentPlayer === 'O' ? 'scale-x-100' : 'scale-x-0'
         }`}
-        viewBox="0 0 40 40"
+        viewBox="0 0 20 20"
       >
         <circle cx="10" cy="10" r="8" stroke="#4B5563" strokeWidth="2" fill="none" />
       </svg>
